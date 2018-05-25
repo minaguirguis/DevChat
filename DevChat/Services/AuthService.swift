3e1238bc84e0c9764d816bb22e98b38593fe7434
Mina Guirguis <MGuirguis.10@gmail.com>
2018-05-25 18:28:27 -0400

    Created FB users

--- DevChat/Services/AuthService.swift
+++ DevChat/Services/AuthService.swift
@@ -30,6 +30,7 @@ class AuthService {
                             } else {
                                 if user?.uid != nil {
                                     //sign in
+                                    DataService.instance.saveUser(uid: user!.uid)
                                     Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
                                         if error != nil {
                                             self.handleFirebaseError(error: error! as NSError, onComplete: onComplete)
